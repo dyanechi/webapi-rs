98f488be19d6299326c5ca1b65c8739df5f79960
dyanechi <dyanechi@proton.me>
2023-02-04 14:01:53 -0500

    update

--- webapi-lib/src/html/tags/link.rs
+++ webapi-lib/src/html/tags/link.rs
@@ -27,10 +27,12 @@ macro_rules! tag_builder {
             pub struct $impl_name { attrs: HashMap<String, String> }
             pub struct [<$impl_name Builder>] { attrs: HashMap<String, String>  }
 
+            // This implementation will change to improve performance
             impl $impl_name {
-                // This implementation will change to improve performance
                 $( pub fn $fn_name(&self) -> &str { self.attrs.get(stringify!($fn_name)).unwrap()} )*
             }
+
+            impl $impl_name { pub fn new() -> [<$impl_name Builder>] { Default::default() } }
             
             impl [<$impl_name Builder>] {
                 $( pub fn $fn_name(mut self, value: &str) -> Self {
@@ -38,10 +40,6 @@ macro_rules! tag_builder {
                     self.attrs.insert(stringify!($fn_name).into(), value.to_string());
                     self
                 } )*
-            }
-
-            impl $impl_name {
-                pub fn new() -> [<$impl_name Builder>] { Default::default() }
             }
 
             impl Default for [<$impl_name Builder>] {
@@ -76,6 +74,65 @@ macro_rules! tag_builder {
 tag_builder!(LinkTag[rel, href, target, media, crossorigin, as_, sizes, referrerpolicy, coords, shape, ping]);
 
 
+// pub struct LinkTagBuilder {
+//     // rel: String,
+//     // href: String,
+//     // target: String,
+//     // media: String,
+//        shape: String,
+//     attrs: HashMap<String, String>
+// }
+// impl Default for LinkTagBuilder {
+//     fn default() -> Self {
+//         let mut attrs = HashMap::new();
+//         attrs.insert("rel".into(), "".into());
+//         attrs.insert("href".into(), "".into());
+//         attrs.insert("target".into(), "".into());
+//         attrs.insert("media".into(), "".into());
+//         Self { attrs }
+//     }
+// }
+// impl Builder<LinkTag> for LinkTagBuilder {
+//     fn build(self) -> LinkTag { LinkTag { attrs: self.attrs } }
+// }
+
+// impl LinkTagBuilder {
+//     pub fn rel(mut self, value: &str) -> Self { self.attrs.insert("rel".into(), value.into()); self }
+//     pub fn href(mut self, value: &str) -> Self { self.attrs.insert("href".into(), value.into()); self }
+//     pub fn target(mut self, value: &str) -> Self { self.attrs.insert("target".into(), value.into()); self }
+//     pub fn media(mut self, value: &str) -> Self { self.attrs.insert("media".into(), value.into()); self }
+//     tag_setter!(crossorigin(value: &str));
+//     tag_setter_fn!(as_);
+// }
+
+
+// pub struct LinkTag {
+//     attrs: HashMap<String, String>
+// }
+// impl LinkTag {
+//     pub fn new() -> LinkTagBuilder { Default::default() }
+// }
+// impl Stringify for LinkTag {
+//     fn stringify(&self) -> String {
+//         let mut link = String::new();
+//         self.attrs.iter().for_each(|(key, val)| {
+//             if !val.is_empty() {
+//                 link.push_str(format!("{}=\"{}\" ", key, val).as_str());
+//             }
+//         });
+
+//         if link.is_empty() { "".to_string() } else {
+//             format!("\n\t<link {}>", link)
+//         }
+//     }
+// }
+// impl LinkTag {
+//     pub fn rel(&self) -> &str { self.attrs.get("rel").unwrap() }
+//     pub fn href(&self) -> &str { self.attrs.get("href").unwrap() }
+//     pub fn target(&self) -> &str { self.attrs.get("target").unwrap() }
+//     pub fn media(&self) -> &str { self.attrs.get("media").unwrap() }
+// }
+
 
 #[cfg(test)]
 mod tests {
@@ -91,6 +148,7 @@ mod tests {
             .href(href)
             .crossorigin(crossorigin)
             .build();
+
 
         assert_eq!(link.rel(), rel);
         assert_eq!(link.href(), href);
--- webapi-lib/src/html/tags/tags.rs
+++ webapi-lib/src/html/tags/tags.rs
@@ -1,3 +1,205 @@
 pub use super::{doc::*, head::*, body::*, link::*};
+use paste::paste;
+
+macro_rules! tag_builder_bc {
+    ( $impl_name:ident [ $( $fn_name:ident ),*  ] ) => {
+        paste! {
+            pub struct $impl_name { attrs: HashMap<String, String> }
+            pub struct [<$impl_name Builder>] { attrs: HashMap<String, String>  }
+
+            // This implementation will change to improve performance
+            impl $impl_name {
+                $( pub fn $fn_name(&self) -> &str { self.attrs.get(stringify!($fn_name)).unwrap()} )*
+            }
+
+            impl $impl_name { pub fn new() -> [<$impl_name Builder>] { Default::default() } }
+            
+            impl [<$impl_name Builder>] {
+                $( pub fn $fn_name(mut self, value: &str) -> Self {
+                    eprintln!("Created: {}", stringify!($fn_name));
+                    self.attrs.insert(stringify!($fn_name).into(), value.to_string());
+                    self
+                } )*
+            }
+
+            impl Default for [<$impl_name Builder>] {
+                fn default() -> Self {
+                    let mut attrs = HashMap::new();
+                    $( attrs.insert(stringify!($fn_name).into(), "".into()); )*
+                    Self { attrs }
+                }
+            }
+
+            impl Builder<$impl_name> for [<$impl_name Builder>] {
+                fn build(self) -> $impl_name { $impl_name { attrs: self.attrs } }
+            }
+
+            impl Stringify for $impl_name {
+                fn stringify(&self) -> String {
+                    let mut s = String::new();
+                    self.attrs.iter().for_each(|(key, val)| {
+                        if !val.is_empty() {
+                            s.push_str(format!("{}=\"{}\" ", key, val).as_str());
+                        }
+                    });
+            
+                    if s.is_empty() { "".to_string() } else {
+                        format!("\n\t<s {}>", s)
+                    }
+                }
+            }
+        }
+    };
+}
+
+macro_rules! build_tag {
+    ( $( $tag_name:ident),* $(,)* ) => {
+        paste! {
+            $( pub struct [<$tag_name Tag>] {} )*
+        }
+    }
+}
+
+macro_rules! rsx {
+    () => {};
+    (
+        $($tag:ident { })*
+    ) => {
+        
+    };
+
+    ( $($tag:ident { $($child_tag:ident { $($tail:tt)* })* })* ) => {
+        $( rsx!($($tail)*) )*;
+    };
+}
+
+build_tag!(
+    A,
+    Area,
+    Audio,
+    Bdi,
+    BlockQuote,
+    Body,
+    Br,
+    Button,
+    Canvas,
+    Caption,
+    Cite,
+    Code,
+    Col,
+    ColGroup,
+    Command,
+    DataList,
+    Dd,
+    Del,
+    Details,
+    Dfn,
+    Dialog,
+    Div,
+    Dl,
+    Dt,
+    Em,
+    Embed,
+    FieldSet,
+    FigCaption,
+    Figure,
+    Footer,
+    Form,
+    H1,
+    H2,
+    H3,
+    H4,
+    H5,
+    H6,
+    Head,
+    Header,
+    Hr,
+    Html,
+    I,
+    IFrame,
+    Img,
+    Input,
+    Ins,
+    Kbd,
+    Label,
+    Legend,
+    Li,
+    Link,
+    Main,
+    Map,
+    Mark,
+    Menu,
+    Meta,
+    Meter,
+    Nav,
+    NoScript,
+    Object,
+    Ol,
+    OptGroup,
+    Option,
+    Output,
+    P,
+    Param,
+    Picture,
+    Pre,
+    Progress,
+    Q,
+    Rb,
+    Rp,
+    Rt,
+    Rtc,
+    Ruby,
+    S,
+    SAmp,
+    Script,
+    Section,
+    Select,
+    Small,
+    Source,
+    Span,
+    Strong,
+    Style,
+    Sub,
+    Summary,
+    Sup,
+    Table,
+    TBody,
+    Td,
+    Template,
+    Textarea,
+    TFoot,
+    Th,
+    THead,
+    Time,
+    Title,
+    Tr,
+    Track,
+    U,
+    Ul,
+    Var,
+    Video,
+    Wbr,
+);
 
 
+rsx!(
+    Div {
+        Div {
+            Div {
+
+            }
+        }
+    }
+);
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tags_build() {
+        let option = OptionTag {};
+    }
+}
