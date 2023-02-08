9cd7693369fcaa8ae3e3f8d07224c5e8a58cbd36
dyanechi <dyanechi@proton.me>
2023-02-07 20:06:35 -0500

    comp

--- webapi-lib/src/html/traits.rs
+++ webapi-lib/src/html/traits.rs
@@ -9,6 +9,27 @@ pub trait Stringify {
     fn stringify(&self) -> String;
 }
 
+pub trait Render {
+    fn render(&self);
+}
+
+pub trait Component: Render + Stringify {
+    fn append(self, other: impl Component);
+}
+
+pub struct Comp {}
+impl Render for Comp {
+    fn render(&self) { }
+}
+impl Stringify for Comp {
+    fn stringify(&self) -> String {
+        "".into()
+    }
+}
+impl Component for Comp {
+    fn append(self, other: impl Component) { }
+}
+
 pub trait HtmlElement {}
 pub trait HtmlEvent {}
 pub trait HtmlNode {}
